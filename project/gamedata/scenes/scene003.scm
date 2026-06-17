63eef9d039e7088d2d6655f38998322c5a32ba1c
pw_32x <git@puffweet.com>
2026-06-16 21:56:07 -0400

    updated scene and map file

--- project/gamedata/scenes/scene003.scm
+++ project/gamedata/scenes/scene003.scm
@@ -8,8 +8,8 @@
     <GameObject Name="Gun Girl" X="28.514729805305777" Y="56.964245316257774" SpawnX="28.514729805305777" SpawnY="56.964245316257774" SpawnPointIsIndependant="False" GameObjectTemplateName="Gun Girl" />
     <Command Name="MapManager_SetTilesetProperties_Command" X="-87.54399999999976" Y="221.6960000000002" SpawnX="-87.54399999999976" SpawnY="221.6960000000002" SpawnPointIsIndependant="False" CommandValue="&amp;setTilesetPropertiesParams " PreCommandData="#include &quot;client/tileset_functions.h&quot;&#xD;&#xA;const SetTilesetProperties_Params setTilesetPropertiesParams = &#xD;&#xA;{ &#xD;&#xA; 1, &#xD;&#xA; breakable_rock_tileset_TilesetFunction, &#xD;&#xA; &amp;rock_particle_template&#xD;&#xA;};" />
     <Command Name="Set Background Vertical Scroll" X="-162" Y="-70" SpawnX="-162" SpawnY="-70" SpawnPointIsIndependant="False" CommandValue="4" PreCommandData="" />
-    <GameObject Name="Wheeler" X="111.75077160493817" Y="82.37525720164585" SpawnX="0" SpawnY="0" SpawnPointIsIndependant="False" GameObjectTemplateName="Wheeler" />
-    <GameObject Name="Wheeler" X="150.66049382716025" Y="89.98392489711904" SpawnX="0" SpawnY="0" SpawnPointIsIndependant="False" GameObjectTemplateName="Wheeler" />
+    <GameObject Name="Wheeler" X="392.75077160493817" Y="61.37525720164585" SpawnX="0" SpawnY="0" SpawnPointIsIndependant="False" GameObjectTemplateName="Wheeler" />
+    <GameObject Name="Wheeler" X="368.6604938271603" Y="14.983924897119039" SpawnX="0" SpawnY="0" SpawnPointIsIndependant="False" GameObjectTemplateName="Wheeler" />
     <GameObject Name="Three Shot Flyer" X="68" Y="41" SpawnX="0" SpawnY="0" SpawnPointIsIndependant="False" GameObjectTemplateName="Three Shot Flyer" />
   </EditorObjects>
 </Scene>
